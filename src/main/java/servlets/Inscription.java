7a672303e274aa70f6707cc85cf3da2ba84cf9a3
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-08-22 22:45:18 +0200

    Initial commit. Project skeleton for students.

---
+++ src/main/java/servlets/Inscription.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Inscription", urlPatterns = {"/inscription"})
+public class Inscription extends HttpServlet {
+
+    /* Des constantes */
+    private static final String VUE = "/WEB-INF/inscription.jsp";
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        /* Affichage de la page d'inscription */
+        this.getServletContext()
+                .getRequestDispatcher(VUE)
+                .forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        throw new ServletException("doPost() not implemented in Inscription!");
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 22 août 2023, 22:38:26
+    Author     : Herbert Caffarel
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Accueil</title>
+    </head>
+    <body>
+        <a href="<c:url value="inscription"/>">Vers la page d'inscription</a>
+    </body>
+</html>
